6a3275c10ecb9e264314ec39f2d95df9a0466bba
Nicolas Esnis <nicolas.esnis@gmail.com>
2018-02-22 12:06:34 +0100

    change meta

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "static#index"
-  get '/main', to: "static#main"
+  get '/main', to: "static#main", as: :main
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
