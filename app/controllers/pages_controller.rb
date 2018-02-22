2bb359b5309fadc25d9888f6622c5951cb3610a9
Nicolas Esnis <nicolas.esnis@gmail.com>
2018-02-22 13:39:39 +0100

    adding weird https file

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+  def letsencrypt
+      # use your code here, not mine
+      render text: "q-0NaguePCP8_318V1lPxWIl1IcgGAKIkhAD_T5VdBw.GnYk0ZVfqR0r5kF9rtzTGzB97uGfxhSEKL2OvzpzUxs"
+    end
+end
