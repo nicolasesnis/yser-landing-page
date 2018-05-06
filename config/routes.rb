5132621e482c96d04810c9080a6e8c070a3b6d81
Nicolas Esnis <nicolas.esnis@gmail.com>
2018-05-06 16:26:01 +0200

    changing to v2

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  root "static#main"
-  get '/main', to: "static#main", as: :main
-  get '/.well-known/acme-challenge/q-0NaguePCP8_318V1lPxWIl1IcgGAKIkhAD_T5VdBw', to: "pages#letsencrypt"
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
