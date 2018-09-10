3184980b45476e21185f8a375e64fac3732b5571
Nicolas Esnis <nicolas.esnis@gmail.com>
2018-09-10 17:32:16 +0200

    big maj

--- mail.php
+++ mail.php
@@ -1,12 +1,20 @@
-<?php $nom = $_POST['nom'];
-$prenom = $_POST['prenom'];
-$telephone = $_POST['telephone'];
-$email = $_POST['email'];
-$entreprise = $_POST['entreprise'];
-$formcontent="Nom: $nom \n Prenom: $prenom \n Telephone: $telephone \n Email: $email \n Entreprise: $entreprise";
-$recipient = "contact@be-yser.com";
-$subject = "Demande Audit RGPD";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+<?php
+
+    ini_set( 'display_errors', 1 );
+
+    error_reporting( E_ALL );
+
+    $from = "nicolas.esnis@gmail.com";
+
+    $to = "nicolas.esnis@be-yser.com";
+
+    $subject = "Vérification PHP mail";
+
+    $message = "PHP mail marche";
+
+    $headers = "From:" . $from;
+
+    mail($to,$subject,$message, $headers);
+
+    echo "L'email a été envoyé.";
 ?>
